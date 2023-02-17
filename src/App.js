d87c570ae69a0b97589da05411d3190cc005be31
peterpazmandi <peter.pazmandi@outlook.hu>
2023-02-17 10:38:05 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+
+const withMousePosition  = (WrappedComponent) => {
+  return (props) => {
+
+    const [mousePosition, setMousePosition] = useState({
+      x: 0, y: 0
+    });
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        })
+      }
+
+      window.addEventListener("mousemove", handleMousePositionChange);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMousePositionChange);
+      }
+    }, []);
+
+    return (
+      <WrappedComponent {...props} mousePosition={mousePosition}/>
+    )
+  }
+}
+
+const PanelMouseLogger = ({ mousePosition }) => {
+  if(!mousePosition) {
+    return null;
+  }
+
+  return (
+    <div>
+      <p>Mouse Position:</p>
+      <div>
+        <span>x: {mousePosition.x}</span>
+        <span>x: {mousePosition.y}</span>
+      </div>
+    </div>
+  )
+}
+
+const PointMouseLogger = ({ mousePosition }) => {
+  if(!mousePosition) {
+    return null;
+  }
+
+  return (
+    <p>(
+      {mousePosition.x}, {mousePosition.y}
+    )</p>
+  )
+}
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        Little Lemon Restaurant
       </header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
   );
 }
